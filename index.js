5f505fb0db7a9962e2627ad44d4ea4d3d820dbab
Adam Wuth <adwu8800@colorado.edu>
2023-07-09 15:08:14 -0600

    index.js changes

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const pgp = require('pg-promise')();
 require('dotenv').config();
 
-// ***********************************
-// <!-- Section 2 : Initialization-->
-// ***********************************
-
 // defining the Express app
 const app = express();
 // using bodyParser to parse JSON in the request body into JS objects
 app.use(bodyParser.json());
+const session = require("express-session");
 // Database connection details
 const dbConfig = {
   host: process.env.POSTGRES_HOST,
@@ -23,7 +21,35 @@ const dbConfig = {
 // Connect to database using the above details
 const db = pgp(dbConfig);
 
+// set session
+app.use(
+  session({
+    secret: "XASDASDA",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+//view engine
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
 
+//Set user vars
+const user = {
+  username: undefined,
+  first_name: undefined,
+  last_name: undefined,
+  email: undefined,
+};
+
+//Login page
+app.get("/login", (req, res) => {
+  res.render("pages/login");
+});
+
+
+
+
+//This goes bottom
 app.listen(4000, () => {
   console.log('listening on port 4000');
 });
