34fdd6bb87c977e83fb978cca06cbfd86469a14c
Aaron Semones <asemones1@icloud.com>
2023-07-21 12:30:13 -0600

    testing stuff

---
+++ All_project_code_components/tests/bdd/stepDefinitions/threadblend.js
@@ -0,0 +1,24 @@
+const { Given, When, Then , And} = require('@cucumber/cucumber');
+
+Given('Given the input of tester2@gmail.com for email and Password_2 for password', function (string) {
+    return 'pending';
+});
+
+
+When('the frontend sends the REST call to database', function () {
+    return 'pending';
+});
+
+
+Then(' the REST call suceeds ', function () {
+    return 'pending';
+});
+
+
+And('the database will authenticate the user', function (string) {
+    return 'pending';
+});
+
+And('and returns the Users home page', function (string) {
+    return 'pending';
+});
