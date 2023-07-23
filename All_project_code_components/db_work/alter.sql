76e0eccead4e61bc47a6a6fe16f9d30ac13b0710
Aaron Semones <asemones1@icloud.com>
2023-07-23 14:43:39 -0600

    commting to rebasE

--- All_project_code_components/db_work/alter.sql
+++ All_project_code_components/db_work/alter.sql
@@ -1,10 +1,13 @@
 
 DROP TABLE "Sender";
-DROP TABLE "Message";
+DROP TABLE "message";
 DROP TABLE "Message_service";
-INSERT INTO "User" ("First_name", "Last_name", "City", "State", "Country", "Email", "Username", "Password") VALUES ('Jimmy', 'Timmy', 'Boulder', 'Colorado', 'US', 'Jimmy@gmail.com', 'Tim', 'password123?');
+/*INSERT INTO "User" ("First_name", "Last_name", "City", "State", "Country", "Email", "Username", "Password") VALUES ('Jimmy', 'Timmy', 'Boulder', 'Colorado', 'US', 'Jimmy@gmail.com', 'Tim', 'password123?');
 INSERT INTO "User" ("First_name", "Last_name", "City", "State", "Country", "Email", "Username", "Password") VALUES ('Limmy', 'Timmy', 'Boulder', 'Colorado', 'US', 'Jimmy@gmail.com', 'Tim', 'password123?');
 INSERT INTO "User" ("First_name", "Last_name", "City", "State", "Country", "Email", "Username", "Password") VALUES ('Ximmy', 'Timmy', 'Boulder', 'Colorado', 'US', 'Jimmy@gmail.com', 'Tim', 'password123?');
 INSERT INTO "User" ("First_name", "Last_name", "City", "State", "Country", "Email", "Username", "Password") VALUES ('Qimmy', 'Timmy', 'Boulder', 'Colorado', 'US', 'Jimmy@gmail.com', 'Tim', 'password123?');
 INSERT INTO "User" ("First_name", "Last_name", "City", "State", "Country", "Email", "Username", "Password") VALUES ('Pimmy', 'Timmy', 'Boulder', 'Colorado', 'US', 'Jimmy@gmail.com', 'Tim', 'password123?');
 INSERT INTO "User" ("First_name", "Last_name", "City", "State", "Country", "Email", "Username", "Password") VALUES ('Zimmy', 'Timmy', 'Boulder', 'Colorado', 'US', 'Jimmy@gmail.com', 'Tim', 'password123?');
+INSERT INTO "Service" ("Name") VALUES ('Discord');
+INSERT INTO "Service" ("Name") VALUES ('Telegram');
+*/
